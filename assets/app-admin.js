d485ca80bc1ca392c06451a025d68b627ce3dba9
web-pub <site.helene2406@gmail.com>
2026-09-03 19:41:35 +0200

    Add files via upload

--- assets/app-admin.js
+++ assets/app-admin.js
@@ -6,7 +6,7 @@ import {
 } from "./firebase-config.js";
 import { meteoPour, alerteMeteo, iconeCode } from "./meteo.js";
 
-const VERSION_SITE = 'V01-001';
+const VERSION_SITE = 'V01-002';
 document.getElementById('versionTag').textContent = VERSION_SITE;
 
 function dateISOLocale(d) {
